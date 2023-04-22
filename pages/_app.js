88200b7eb6223cdba7c5d5e8d9d795f761a115f6
Racknaro <racknarow1@gmail.com>
2023-04-22 01:59:37 -0500

    add boostrap

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 import store from '@/store';
 import '@/styles/globals.scss';
-if (typeof window !== 'undefined') {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+/* if (typeof window !== 'undefined') {
     // Cargar estilos de Boostrap en el cliente
     import('bootstrap/dist/css/bootstrap.min.css');
     import('bootstrap/dist/js/bootstrap.bundle.min');
-};
+}; */
 
 export default function App({ Component, pageProps }) {
 
