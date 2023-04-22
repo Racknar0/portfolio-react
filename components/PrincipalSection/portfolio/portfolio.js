6af4e57bb0a047e5a58a8ce6135409bad471daa5
Racknaro <racknarow1@gmail.com>
2023-04-22 18:18:20 -0500

    modify projects

--- components/PrincipalSection/portfolio/portfolio.js
+++ components/PrincipalSection/portfolio/portfolio.js
@@ -3,32 +3,51 @@ import Card from './card/card';
 import styles from '@/styles/components/portfolio.module.scss';
 
 import { proyectos } from '@/db/projects';
+import { useState } from 'react';
+
 
 const Portfolio = () => {
 
     const { lang } = useSelector(state => state.lang);
 
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const handleExpand = () => {
+        setIsExpanded(!isExpanded);
+        console.log('isExpanded: ', isExpanded);
+    };
+
     return (
         <div
             id="portfolio"
-            className={`${styles.contenedor_portfolio} d-flex justify-content-center align-items-center`} /* data-aos="fade-up"
+            className={`${styles.contenedor_portfolio} d-flex flex-column justify-content-center align-items-center`} /* data-aos="fade-up"
             data-aos-duration="1000" */
         >
             <div className="container-xl">
+
                 <h2 className="text-white text-center">
                     <span>{lang.portfolio}</span>
                 </h2>
-                <p
-                    className={`${styles.portfolio_text_1} fs-1 text-center mt-5 pt-5`}
-                >
+                <p className={`${styles.portfolio_text_1} fs-1 text-center mt-5 pt-5`} >
                     {lang.portfolio1}
                 </p>
-                <div className={`row ${styles.grid_services}`}>
-                    {proyectos.map((proyecto, index) => {
-                            return (
-                                <Card proyecto={proyecto} key={ index } />
-                            );
+
+                <button
+                    className={`${styles.button_expand}`}
+                    onClick={handleExpand}
+                >
+                    {isExpanded ? 'Ver menos' : 'Ver más'}
+                </button>
+
+                <div className={`${isExpanded ? styles.expanded : styles.collapsed}`} >
+
+                    <div className={`row ${styles.grid_services}`} >
+                        {proyectos.map((proyecto, index) => {
+                            return <Card proyecto={proyecto} key={index} />;
                         })}
+                    </div>
+
                 </div>
             </div>
         </div>
---
+++ pages/certificates.js
@@ -0,0 +1,12 @@
+import Header from '@/components/header/header';
+import React from 'react';
+
+const Certificates = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+};
+
+export default Certificates;
